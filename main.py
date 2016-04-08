b0766625a0ba31a50ad6f9e3a0b6afbd375eaeeb
Lasse Ebdrup Pedersen <laeb@biosustain.dtu.dk>
2016-04-09 00:31:16 +0200

    changed to pparps old method of reading sams. Much faster and memory efficient.

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ import pandas as pd
 from Bio import SeqIO
 
 
-fasta_fn = "/home/laeb/data/storage/NGS/others/Florian/ACC1.fa"
+#fasta_fn = "/home/laeb/data/storage/NGS/others/Florian/ACC1.fa"
+fasta_fn = "/home/laeb/Downloads/ACC1.fa"
 fasta = str(SeqIO.read(open(fasta_fn), 'fasta').seq)
 
 try:
     df = pd.read_pickle("lowmut")
 except FileNotFoundError:
-    align_fn = "/home/laeb/data/storage/NGS/others/Florian/bow/lowmut.bam"
-    df = sam2df(align_fn, fasta)
+    #align_fn = "/home/laeb/data/storage/NGS/others/Florian/bow/lowmut.bam"
+    align_fn = "/home/laeb/Downloads/lowmut.sam"
+    df = sam2df(align_fn, len(fasta))
     df.to_pickle("lowmut")
-
 
 plt.style.use(['ggplot'])
 plots.coverage_plot(df)
 plt.show()
 #plots.seq_plot(df, fasta, 100, 200)
-#plots.mut_plot(df, fasta)
-#plt.show()
+plots.mut_plot(df, fasta)
+plt.show()
 
--- sam2df.py
+++ sam2df.py
@@ -1,52 +1,32 @@
 from pandas import DataFrame
 
 
-def sam2df(sam_fn, fasta):
-    df = DataFrame(index=range(len(fasta)), columns=['A', 'T', 'C', 'G', '^', '+', 'N']).fillna(0)
-
+def sam2df(sam_fn, fasta_len):
     reads = 0
+    DI_reads = 0
+    dna_data = []
+    for x in range(fasta_len):
+        dna_data.append({'A': 0, 'G': 0, 'C': 0, 'T': 0, 'N': 0})
+
     with open(sam_fn) as f:
         for read in f:
             if not read[0] == '@':
                 parts = read.split("\t")
                 flag = int(parts[1])
                 reads += 1
-                if reads % 10 == 0:
+                if reads % 100000 == 0:
                     print(reads)
 
                 if flag in [83, 99, 147, 163]:
-                    df_pos = int(parts[3]) - 1
                     cigar = parts[5]
-                    seq = parts[9]
+                    if "I" in cigar or "D" in cigar:
+                        DI_reads += 1
+                    else:
+                        pos = int(parts[3]) - 1
+                        seq = parts[9]
 
-                    read_pos = 0
+                        for x in range(len(seq)):  # go over the positions in the dna
+                            dna_data[x+pos][seq[x]] += 1  # write WT
 
-                    for code, l in read.cigartuples:
-                        # M 0 alignment match (can be a sequence match or mismatch)"
-                        # I 1 insertion to the reference
-                        # D 2 deletion from the reference
-                        # N 3 skipped region from the reference
-                        # S 4 soft clipping (clipped sequences present in SEQ)
-                        # H 5 hard clipping (clipped sequences NOT present in SEQ)
-                        # P 6 padding (silent deletion from padded reference)
-                        # = 7 sequence match
-                        # X 8 sequence mismatch
-
-                        if code not in [0, 1, 2]:
-                            print("WARNING, unhandled cigar code: {}, {}".format(code, read.cigarstring))
-                        else:
-                            if code == 0:  # match, mark df_pos with base at read_pos and move both ahead
-                                for tracker in range(l):
-                                    df.loc[df_pos, read.seq[read_pos]] += 1
-                                    df_pos += 1
-                                    read_pos += 1
-                            elif code == 1:  # insertion, mark df_pos as insert and move read_pos ahead
-                                df.loc[df_pos, '+'] += 1
-                                for tracker in range(l):
-                                    read_pos += 1
-                            elif code == 2:  # deletion, mark df_pos with deletion at each pos and move df_pos ahead
-                                for tracker in range(l):
-                                    df.loc[df_pos, '^'] += 1
-                                    df_pos += 1
-
-    return DataFrame(df)
+    print("{} reads, {} DI, {:.2%}".format(reads, DI_reads, DI_reads/reads))
+    return DataFrame(dna_data)

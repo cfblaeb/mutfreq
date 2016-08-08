0c397b3dd101d0478f310d1ac06f519554b46cd0
laeb <laeb@biosustain.dtu.dk>
2016-08-08 09:29:49 +0200

    few changes

--- plots.py
+++ plots.py
@@ -9,7 +9,7 @@ def coverage_plot(df, ax):
 
 
 def seq_plot(df, ax, fasta, start, end):
-    df = set_fasta_index(df, fasta)
+    df = set_fasta_index(df.copy(), fasta)
 
     df[start:end].plot(ax=ax, kind='bar', stacked=True, title='seq plot')
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
@@ -17,7 +17,7 @@ def seq_plot(df, ax, fasta, start, end):
 
 
 def mut_plot_sum(df, ax, fasta):
-    df = set_fasta_index(df, fasta)
+    df = set_fasta_index(df.copy(), fasta)
 
     df2 = (df.T / df.T.sum()).T
     df2.loc['A', 'A'] = 0
@@ -29,11 +29,11 @@ def mut_plot_sum(df, ax, fasta):
     df3 = df2.set_index('i')
     df3.T.sum().plot(ax=ax, logy=True, title='Mutation frequency per position')
     ax.set_xlabel('Position')
-    ax.set_ylabel('Frequency of mutations (0 to 1, log scale)')
+    ax.set_ylabel('Frequency of mutations')
 
 
 def mut_plot(df, ax, fasta):
-    df = set_fasta_index(df, fasta)
+    df = set_fasta_index(df.copy(), fasta)
 
     df2 = (df.T / df.T.sum()).T
     df2.loc['A', 'A'] = 0
